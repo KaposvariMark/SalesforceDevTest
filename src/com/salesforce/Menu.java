be7bc65f53517cc3ccdb88183dca3dd83ee0ab76
Márk Kaposvári <kaposvari.markz@gmail.com>
2021-10-09 16:18:32 +0200

    SFDT07 added submenu and contact entitity modification

--- src/com/salesforce/Menu.java
+++ src/com/salesforce/Menu.java
@@ -1,26 +1,54 @@
 package com.salesforce;
 
+import com.salesforce.entities.Account;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Menu {
 
-    public int printMainOptions(){
-        int selection = 5;
-        Scanner sc = new Scanner(System.in);
+    public static final String PLEASE_CHOSE_A_NUMBER_FROM_THE_ALIGNING_OPTIONS = "Please chose a number from the aligning options";
+
+    public int printMainOptions(Scanner sc){
+        int selectedOption = 0;
+
         System.out.println("\n--------OPTIONS--------");
         System.out.println("1. List accounts");
         System.out.println("2. List contacts");
-        System.out.println("3. List contacts by account");
+        System.out.println("3. Get contacts of account");
         System.out.println("4. Exit");
         System.out.println("-----------------------");
 
         System.out.print("Chosen option: ");
         try {
-            selection = sc.nextInt();
+            selectedOption = sc.nextInt();
         } catch (Exception e){
-            System.out.println("Please chose a number from the aligning options");
+            System.out.println(PLEASE_CHOSE_A_NUMBER_FROM_THE_ALIGNING_OPTIONS);
         }
+        System.out.print("\n********************************************\n");
+        return selectedOption;
+    }
 
-        return selection;
+    public Account printAccountsOptions(Scanner sc, ArrayList<Account> Accounts){
+        int selectedOption = -1;
+        boolean numberIsIncorrect = true;
+
+        do {
+            System.out.println("\n-------ACCOUNTS--------");
+            for (int i = 0; i < Accounts.size(); i++) {
+                System.out.println((i+1) + ". " + Accounts.get(i).getName());
+            }
+            System.out.println("-----------------------");
+
+            System.out.print("Chosen option: ");
+            try {
+                selectedOption = sc.nextInt() - 1;
+            } catch (Exception e){
+                System.out.println(PLEASE_CHOSE_A_NUMBER_FROM_THE_ALIGNING_OPTIONS);
+            }
+            if(selectedOption < Accounts.size() && selectedOption >= 0) numberIsIncorrect = false;
+        } while (numberIsIncorrect);
+        System.out.print("\n********************************************\n");
+        return Accounts.get(selectedOption);
     }
 }
--- src/com/salesforce/entities/Contact.java
+++ src/com/salesforce/entities/Contact.java
@@ -4,18 +4,25 @@ public class Contact {
 
     private String id;
 
+    private String accountID;
+
     private String firstName;
 
     private String lastName;
 
-    public Contact(String id, String firstName, String lastName) {
+    public Contact(String id, String accountID, String firstName, String lastName) {
         this.id = id;
+        this.accountID = accountID;
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
     public String getId() {
         return id;
+    }
+
+    public String getAccountID() {
+        return accountID;
     }
 
     public String getFirstName() {
